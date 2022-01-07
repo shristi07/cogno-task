4325a1aee0e60c46de6d7a2ba7f6850dae34ac4c
shristi <shristikatiyar1@gmail.com>
2022-01-08 03:58:11 +0530

    first-commit

---
+++ bot.js
@@ -0,0 +1,10 @@
+function handleClose() {
+    var x = document.getElementById("myDIV");
+    var z = document.getElementById("myIcon");
+    x.style.display = "none"; 
+}
+function handleOpen() {
+    var y = document.getElementById("myDIV");
+    y.style.display = "unset";  
+    
+}
